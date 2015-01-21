ba21e49d262c5e0ae946afd5d9fcb867230b6995
Ben McGunigle <bnmcgn@gmail.com>
2015-01-20 20:35:10 -0800

    Added tests for itertools.lisp

--- tests.lisp
+++ tests.lisp
@@ -93,3 +93,123 @@
 		    (do-generators ((g (some-numbers))
 				    (:list l data :fill-value 0))
 		      (yield (+ l g)))))))))
+
+(test icount
+  (is (equal '(10 12 14 16)
+	     (take 4
+		   (icount 10 2)))))
+
+(test cycle
+  (is (= 163
+       (reduce #'+
+	       (take 30 
+		(cycle '(4 5 6 7)))))))
+
+(test repeat 
+  (is (= 20
+	 (reduce #'+
+		 (generator->list
+		  (repeat 2 10))))))
+
+(test chain
+  (is (= 66
+       (reduce #'+
+	       (generator->list
+		(chain (some-numbers) (some-numbers)))))))
+
+(test izip-longest
+  (is (= 179
+	 (let ((x 0))
+	   (do-generator (a b 
+			    (izip-longest 
+			     (list->generator data) 
+			     (some-numbers) :fill-value 10))
+	     (incf x (* a b)))
+	   x))))
+
+(test compress
+  (is (equal '(a c e f)
+	     (generator->list
+	      (compress (list->generator '(a b c d e f))
+			(list->generator '(t nil t nil t t)))))))
+
+(test dropwhile
+  (is (equal '(6 4 1)
+	     (generator->list
+	      (dropwhile (lambda (x) (< x 5))
+			 (list->generator '(1 4 6 4 1)))))))
+
+(test takewhile
+  (is (equal '(1 4)
+	     (generator->list
+	      (takewhile (lambda (x) (< x 5))
+			 (list->generator '(1 4 6 4 1)))))))
+
+(test groupby
+  (is (= 4
+	 (length (car 
+	   (nth-value 1 (take 1 
+			      (groupby 
+			       (list->generator '(A A A A B B B C C))))))))))
+
+(test ifilter
+  (is (= 20
+	 (reduce #'+ (generator->list
+		      (ifilter #'evenp (some-numbers))))))
+  (is (= 13
+	 (reduce #'+ (generator->list
+		      (ifilter-false #'evenp (some-numbers)))))))
+
+(test islice
+  (is (equal '(a b)
+	     (generator->list (islice (list->generator '(a b c d e f g)) 2))))
+  (is (equal '(c d)
+	     (generator->list (islice (list->generator '(a b c d e f g)) 2 4))))
+  (is (equal '(c d e f g)
+	     (generator->list 
+	      (islice (list->generator '(a b c d e f g)) 2 nil))))
+  (is (equal '(a c e g)
+	     (generator->list
+	      (islice (list->generator '(a b c d e f g)) 0 nil 2)))))
+
+(test imap 
+  (is (equal '(8 15 24)
+	 (generator->list 
+	  (imap #'* (list->generator  '(2 3 4)) (list->generator '(4 5 6)))))))
+
+(test starmap
+  (is (equal '(32 9 1000)
+	 (generator->list (starmap #'expt (list->generator 
+					   '((2 5) (3 2) (10 3))))))))
+
+(test tee
+  (is (= 33
+	 (reduce #'+
+		 (multiple-value-bind
+		       (a b) (tee (some-numbers) 2)
+		   (consume 6 a)
+		   (generator->list b))))))
+
+(test product
+  (is (equal '(1 1 1 1 1 2)
+	     (take 6
+		   (product data data)))))
+
+(test permutations
+  (is (equal '((1 2))
+	     (take 1
+		   (let ((*pygen-multi-mode* :list))
+		     (permutations data 2))))))
+
+(test combinations-with-replacement
+  (is (= 6 
+	 (length (nth-value 1
+			    (generator->list
+			     (combinations-with-replacement
+			      '(2 3 4) 2)))))))
+
+(test combinations
+  (is (= 3 (length (nth-value 1
+			      (generator->list
+			       (combinations
+				'(2 3 4) 2)))))))
